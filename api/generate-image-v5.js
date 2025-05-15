f79812f4a79efe632a83703cd846ac36a9227bbd
ethelise <aren0r.w@gmail.com>
2025-05-15 07:04:50 +0100

    Update generate-image-v5.js

--- api/generate-image-v5.js
+++ api/generate-image-v5.js
@@ -32,7 +32,7 @@ module.exports = async function handler(req, res) {
             model: 'gpt-image-1',
             prompt: prompt,
             n: 1,
-            size: '512x512',
+            size: '1024x1024',
             output_format: 'jpeg', // corrected property name from output_format
             quality: 'low'
           }),
